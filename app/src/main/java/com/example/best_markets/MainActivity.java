3983dc79c1936eb85178198630f6a1fe1abea636
hyebin0411 <4444ssome@gmail.com>
2022-04-10 13:35:10 +0900

    shoplist 관련 업데이트

--- app/src/main/java/com/example/best_markets/MainActivity.java
+++ app/src/main/java/com/example/best_markets/MainActivity.java
@@ -22,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- app/src/main/java/com/example/best_markets/MenuFragment.java
+++ app/src/main/java/com/example/best_markets/MenuFragment.java
@@ -9,13 +9,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MenuFragment extends Fragment {
+    static final String[] LIST_MENU = {"menu1", "menu2", "menu3"};
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_menu, null);
+
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) view.findViewById(R.id.listview);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String strText = (String) parent.getItemAtPosition(position);
+            }
+        });
+
+        return view;
+
+        //return inflater.inflate(R.layout.fragment_menu, container, false);
     }
 }
