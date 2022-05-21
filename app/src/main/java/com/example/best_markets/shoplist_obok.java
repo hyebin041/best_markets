a2822c1747c248fefd0c12e047a40f8a8808d272
eunju00 <84320502+eunju00@users.noreply.github.com>
2022-05-21 23:07:36 +0900

    shoplist_
    가게 목록과 한줄평 업로드

---
+++ app/src/main/java/com/example/best_markets/shoplist_obok.java
@@ -0,0 +1,56 @@
+package com.example.best_markets;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.RatingBar;
+
+import com.example.best_markets.shoplist_fragments.InformationFragment;
+import com.example.best_markets.shoplist_fragments.MenuFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class shoplist_obok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoplist_obok);
+
+        RatingBar ratingBar = findViewById(R.id.ratingBar1);
+        ratingBar.bringToFront();
+
+        // 프래그먼트 객체 선언
+        Fragment fragment_menu_obok = new MenuFragment_obok();
+        Fragment fragment_inform_obok = new InformationFragment_obok();
+
+
+        //제일 처음 띄워줄 뷰를 세팅해줍니다. commit();까지 해줘야 합니다.
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment_menu).commitAllowingStateLoss();
+
+        // 바텀 네비게이션 객체 선언
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // 바텀 네비게이션 클릭 리스너 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.fragment_menu_obok:
+                        // replace(프레그먼트를 띄워줄 frameLayout, 교체할 fragment 객체)
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment_menu).commitAllowingStateLoss();
+                        return true;
+                    case R.id.fragment_information_obok:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment_inform).commitAllowingStateLoss();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+    }
+}
