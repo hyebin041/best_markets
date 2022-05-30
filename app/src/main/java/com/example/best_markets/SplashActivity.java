7c5f546065986e37cacd19d4ae2c32b419f098da
eunju00 <84320502+eunju00@users.noreply.github.com>
2022-05-30 21:31:42 +0900

    splash
    splash화면 완료

--- app/src/main/java/com/example/best_markets/SplashActivity.java
+++ app/src/main/java/com/example/best_markets/SplashActivity.java
@@ -1,5 +1,5 @@
 package com.example.best_markets;
-/*
+
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import android.app.Activity;
@@ -21,7 +21,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.activity_splash);
 
         constraintLayout=findViewById(R.id.constraintLayout);
         market_jjang1 = findViewById(R.id.market_jjang1);
@@ -38,7 +38,7 @@ public class SplashActivity extends Activity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                startActivity(new Intent(SplashActivity.this, Shop.class));
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
 
             @Override
@@ -49,4 +49,90 @@ public class SplashActivity extends Activity {
         market_jjang1.startAnimation(anim_FadeIn);
         market_jjang2.startAnimation(anim_ball);
     }
-}*/
+}
+
+/*import androidx.constraintlayout.widget.ConstraintLayout;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity {
+    Animation anim_FadeIn;
+    Animation anim_ball;
+    ConstraintLayout constraintLayout;
+    ImageView lcklockImageView;
+    ImageView oImageView;
+    ImageView faceRecgnitionImageView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        constraintLayout=findViewById(R.id.constraintLayout);
+        lcklockImageView=findViewById(R.id.lock_lck);
+        oImageView=findViewById(R.id.imageView);
+        faceRecgnitionImageView=findViewById(R.id.faceReconition);
+
+
+        anim_FadeIn=AnimationUtils.loadAnimation(this,R.anim.splash_fadeln);
+        anim_ball=AnimationUtils.loadAnimation(this,R.anim.splash_ball);
+
+
+        anim_FadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        lcklockImageView.startAnimation(anim_FadeIn);
+        faceRecgnitionImageView.startAnimation(anim_FadeIn);
+        oImageView.startAnimation(anim_ball);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);	//theme로 지정했다면 삭제한다.
+
+        moveMain(1);	//1초 후 main activity 로 넘어감
+    }
+
+    private void moveMain(int sec) {
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                //new Intent(현재 context, 이동할 activity)
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(intent);	//intent 에 명시된 액티비티로 이동
+
+                finish();	//현재 액티비티 종료
+            }
+        }, 1000 * sec); // sec초 정도 딜레이를 준 후 시작
+    }
+
+
+}
+*/
